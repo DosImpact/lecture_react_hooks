95950a7b2fa86ce05f40d0c0d372fe39a13aafab
DosImpact <ypd03008@gmail.com>
2020-04-12 01:11:20 +0900

    fisrt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Hook1 = () => <div>HOOK-1</div>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DOSHOOKS</h1>
+        <Hook1 />
       </header>
     </div>
   );
