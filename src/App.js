3e7d0611e055b8d2f4e6d7134aae65180fe5ec27
DosImpact <ypd03008@gmail.com>
2020-04-12 01:23:01 +0900

    done #1.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const Hook1 = () => <div>HOOK-1</div>;
+const Section = ({ title, children }) => (
+  <div>
+    <h2>{title}</h2>
+    {children}
+  </div>
+);
+
+const Hook1 = () => {
+  const [count, setCount] = useState(1);
+  const plus = () => setCount(count + 1);
+  const minus = () => setCount(count - 1);
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>DOSHOOKS</h1>
-        <Hook1 />
+        <h1>DOS 🛠 HOOKS</h1>
+        <Section title={"USE STATE"}>
+          <Hook1 />
+        </Section>
       </header>
     </div>
   );
